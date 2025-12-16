8957d34275969be9a820878016b6867396ed42a0
trig001 <trig001@gmail.com>
2025-12-16 10:36:13 -0600

    initial functionality of the time

--- StudyTimer.java
+++ StudyTimer.java
@@ -12,14 +12,28 @@ public class StudyTimer {
         System.out.println();
 
         System.out.print("Welcome to your trust study timer. Just set the time\n" +
-                           "you want to study and the alarm will let you know\n" +
-                           "when you are done.\n");
+                         "you want to study and the alarm will let you know\n" +
+                         "when you are done.\n");
         System.out.println();
 
-        System.out.print("Please enter the time (in minutes) you need to Study: ");
-        double timer = scnr.nextDouble();
+        int timer;
+        do {
+            System.out.print("Please enter the time (in minutes) you need to Study: ");
+            timer = scnr.nextInt();
 
-        
+            if(timer < 1 || timer > 120) {
+                System.out.println("Please enter time between 1-120 minutes.");
+            }
+        } while (timer < 1 || timer >120);
+
+        for (int timeLeft = timer; timeLeft > 0; timeLeft --) {
+
+            try {
+                Thread.sleep(1000);
+            } catch(InterruptedException e) {
+                System.out.println("Studying Done");
+            }
+        }
 
     }
     
