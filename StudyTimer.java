042dffb8b8e0c8d336c7c2ae8dd46353a8253691
trig001 <trig001@gmail.com>
2025-12-16 10:16:05 -0600

    building the timer, and practicing version control

---
+++ StudyTimer.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class StudyTimer {
+    public static void timer(Scanner scnr) {
+
+        System.out.printf(" \n" +
+                          "+-----------------+\n" +
+                          "|                 |\n" +
+                          "|   Study Timer   |\n" +
+                          "|                 |\n" +
+                          "+-----------------+\n"
+        );
+        System.out.println();
+
+        System.out.print("Welcome to your trust study timer. Just set the time\n" +
+                           "you want to study and the alarm will let you know\n" +
+                           "when you are done.\n");
+        System.out.println();
+
+        System.out.print("Please enter the time you need to Study: ");
+        double timer = scnr.nextDouble();
+
+        
+    }
+    
+}
