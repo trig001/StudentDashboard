b1ea7d94b0d8f2e63fdf2d6cbdd0c8ab799516f6
trig001 <trig001@gmail.com>
2025-12-17 06:54:52 -0600

    Completed the StudyTimer.java

--- StudyTimer.java
+++ StudyTimer.java
@@ -21,26 +21,25 @@ public class StudyTimer {
         do {
             System.out.print("Please enter the time (in minutes) you need to Study: ");
             timer = scnr.nextInt();
+            scnr.nextLine();
 
             if(timer < 1 || timer > 120) {
                 System.out.println("Please enter time between 1-120 minutes.");
             }
         } while (timer < 1 || timer >120);
 
-        double quarter = timer / .25;
+        double quarter = timer / 4;
         int checkpoint = (int) Math.round(timer - quarter);
         int remainingPercent = 100;
 
         for (int timeLeft = timer; timeLeft > 0; timeLeft --) {
 
-            if (quarter > 0 && timeLeft == checkpoint) {
-                remainingPercent -= 25;
-                System.out.println(remainingPercent + "% remaining");
-                checkpoint -= quarter;
-            }
-
-            if (timeLeft == (timer / 2)) {
-                System.out.println("You have been studing hard, maybe its time for a break?");
+            if (timeLeft == Math.round(timer * .75)) {
+                System.out.println("75% time remaining, maybe it's time to take a break.");
+            } else if(timeLeft == Math.round(timer * .5)) {
+                System.out.println("50% of your study time is remaining, should we take another break?");
+            } else if(timeLeft == Math.round(timer * .25)) {
+                System.out.println("25% time remaining, it's ok to take one last break before reaching the end!");
             }
 
             System.out.printf("You have %d minutes left.\n", timeLeft);
@@ -49,8 +48,30 @@ public class StudyTimer {
                 Thread.sleep(1000);
             } catch(InterruptedException e) {
                 System.out.println("Studying Done");
-            }
+            } 
         }
+        System.out.println();
+        System.out.printf("Session completed you studied for %d minutes\n", timer);
+        System.out.println("Great Work");
+
+        System.out.println();
+
+        String menuAnswer;
+
+        do{ 
+            System.out.print("Return to menu? (yes/no) ");
+            
+            menuAnswer = scnr.nextLine().toLowerCase();;
+
+
+            if (menuAnswer.equals("yes")) {
+                StudentDashboard.dashboardMenu();
+            } else if (menuAnswer.equals("no")) {
+                System.out.println("Good luck with your studies! Goodbye!");
+                StudentDashboard.exitMenu = true;
+            }
+        } while(!menuAnswer.equals("yes") && !menuAnswer.equals("no"));
+        
 
     }
     
