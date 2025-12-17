53a1e9b1b80b2ec0c739fbbfad959d308537de71
trig001 <trig001@gmail.com>
2025-12-17 12:12:22 -0600

    Corrected some typos and added validation for the Days, Points, Difficult prompts

--- AssignmentPriorityChecker.java
+++ AssignmentPriorityChecker.java
@@ -18,18 +18,39 @@ public class AssignmentPriorityChecker {
                          
         System.out.println();
 
-        double points = 0;
-        double days = 0;
-        double difficulty = 0;
+        double points;
+        double days;
+        double difficulty;
 
         System.out.print("Enter assignment name: ");
         String assignmentName = scnr.nextLine();
-        System.out.print("Enter days left: ");
-        days = scnr.nextDouble();
-        System.out.print("Enter total points: ");
-        points = scnr.nextDouble();
-        System.out.print("Enter level of difficulty: ");
-        difficulty = scnr.nextDouble();
+
+        do {
+            System.out.print("Days until due (1-30): ");
+            days = scnr.nextDouble();
+            
+            if (days < 1 || days > 30) {
+                System.out.println("Days must be between 1-30!");
+            }
+        } while (days < 1 || days > 30);
+
+        do {
+            System.out.print("Enter total points (1-500): ");
+            points = scnr.nextDouble();
+            
+            if (points < 1 || points > 500) {
+                System.out.println("Points must be between 1-500!");
+            }
+        } while (points < 1 || points > 500);
+
+        do {
+            System.out.print("Enter level of difficulty (1-10): ");
+            difficulty = scnr.nextDouble();
+            
+            if (difficulty < 1 || difficulty > 10) {
+                System.out.println("Difficulty must be between 1-10!");
+            }
+        } while (difficulty < 1 || difficulty > 10);
 
         double priorityScore = ((points / days) + (difficulty * 5));
 
@@ -40,7 +61,7 @@ public class AssignmentPriorityChecker {
         System.out.println("____________________________");
         System.out.println();
 
-        System.out.printf("Priority Score: %.0f\n", priorityScore);
+        System.out.printf("Priority Score: %.2f\n", priorityScore);
         if(priorityScore >= 80) {
             System.out.println("Urgency: High ⚠️");
         } else if(priorityScore <80 && priorityScore >=50) {
@@ -55,7 +76,7 @@ public class AssignmentPriorityChecker {
             System.out.println("Recommendation: Start this immediately!!");
             System.out.println("This is a high-priority assignment.");
         } else if(priorityScore <80 && priorityScore >=50) {
-            System.out.println("Recommendation: Should be ready to start this if you havent already.");
+            System.out.println("Recommendation: Should be ready to start this if you haven't already.");
             System.out.println("This is a medium-priority assignment.");
         } else if(priorityScore < 50) {
             System.out.println("You have time, but don't procrastinate.");
